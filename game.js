1900bb1318c90ca2a1418b3028f96ecc0a04d96c
Jorge Sevilla <jorge.garcia@compasso.com.br>
2023-01-18 16:25:05 -0300

    Canvas responsive

--- game.js
+++ game.js
@@ -1,51 +1,47 @@
-const canvas = document.querySelector('#game');
-const game = canvas.getContext('2d');
+const canvas = document.querySelector("#game");
+const game = canvas.getContext("2d");
 
 let canvasSize;
 let elementsSize;
 
-window.addEventListener('load', startGame);
-window.addEventListener('resize', startGame);
+window.addEventListener("load", setCanvasSize);
+window.addEventListener("resize", setCanvasSize);
 
-function startGame(){
+function setCanvasSize() {
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
 
-    setCanvasSize();
-  
-    canvas.setAttribute('width', canvasSize);
-    canvas.setAttribute('height', canvasSize);   
+  canvas.setAttribute("width", canvasSize);
+  canvas.setAttribute("height", canvasSize);
 
-    console.log({canvasSize, elementsSize })
-    
-    game.font = elementsSize + 'px Verdana';
-    game.textAlign = 'end';
+  elementsSize = canvasSize / 10;
 
-    for (let i = 1; i <= 10; i++) {
-        game.fillText(emojis['X'], elementsSize , elementsSize * i);
-        
-    }
-
-    function setCanvasSize(){
-        
-
-        if(window.innerHeight > window.innerWidth){
-            canvasSize = window.innerWidth * 0.8;
-        }else {
-            canvasSize = window.innerHeight * 0.8;
-        }
-    }
-
-
-    
-  // window.innerHeight
-  // window.innerWidth
-  
-  // game.fillRect(0,50,100,100);
-  // game.clearRect(50,50,50,50);
-  // game.clearRect()
-  // game.clearRect(0,0,50,50);
-
-  // game.font = '25px Verdana'
-  // game.fillStyle = 'purple';
-  // game.textAlign = 'center';
-  // game.fillText('Platzi', 25, 25);
+  startGame();
 }
+
+function startGame() {
+  console.log({ canvasSize, elementsSize });
+
+  game.font = elementsSize + "px Verdana";
+  game.textAlign = "end";
+
+  for (let i = 1; i <= 10; i++) {
+    game.fillText(emojis["X"], elementsSize, elementsSize * i);
+  }
+}
+
+// window.innerHeight
+// window.innerWidth
+
+// game.fillRect(0,50,100,100);
+// game.clearRect(50,50,50,50);
+// game.clearRect()
+// game.clearRect(0,0,50,50);
+
+// game.font = '25px Verdana'
+// game.fillStyle = 'purple';
+// game.textAlign = 'center';
+// game.fillText('Platzi', 25, 25);
