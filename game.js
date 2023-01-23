4f0930b77f198b699c3634ea8c2d8790400e2222
Jorge Sevilla <jorge.garcia@compasso.com.br>
2023-01-23 16:57:42 -0300

    ForEach y objeto playerPosition

--- game.js
+++ game.js
@@ -1,5 +1,9 @@
 const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
+const btnUp = document.querySelector("#up");
+const btnLeft = document.querySelector("#left");
+const btnRight = document.querySelector("#right");
+const btnDown = document.querySelector("#down");
 
 let canvasSize;
 let elementsSize;
@@ -28,9 +32,55 @@ function startGame() {
   game.font = elementsSize + "px Verdana";
   game.textAlign = "end";
 
-  for (let i = 1; i <= 10; i++) {
-    game.fillText(emojis["X"], elementsSize, elementsSize * i);
+  const map = maps[2];
+  const mapRows = map.trim().split('\n');
+  const mapRowCols = mapRows.map(row => row.trim().split(''));
+
+  mapRowCols.forEach((row, rowI) => {
+    row.forEach((col, colI) => {
+      const emoji = emojis[col];
+      const posX = elementsSize * (colI + 1);
+      const posY = elementsSize * (rowI + 1);
+      game.fillText(emoji, posX, posY)
+    });
+  });
+
+  window.addEventListener('keydown', moveByKeys);
+  btnUp.addEventListener('click', moveUp);
+  btnLeft.addEventListener('click', moveLeft);
+  btnRight.addEventListener('click', moveRight);
+  btnDown.addEventListener('click', moveDown);
+
+  function moveByKeys(event){
+    if(event.key == 'ArrowUp') moveUp;
+    else if(event.key == 'ArrowLeft') moveLeft;
+    else if(event.key == 'ArrowRight') moveRight;
+    else if(event.key == 'ArrowDown') moveDown;
   }
+
+  function moveUp(){
+
+  }
+
+  function moveLeft(){
+    
+  }
+
+  function moveRight(){
+    
+  }
+
+  function moveDown(){
+    
+  }
+
+  /*for (let row = 1; row <= 10; row++) {
+    for (let col = 1; col <= 10; col++) {    
+        game.fillText(emojis[mapRowCols[row - 1] [col - 1]],
+             elementsSize * col, elementsSize * row);
+    }
+    
+  }*/
 }
 
 // window.innerHeight
